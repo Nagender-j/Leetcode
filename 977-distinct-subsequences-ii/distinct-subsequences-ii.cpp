f163af0d4326c7500cb1dc494a5e66e642e8a837
Nagender-j <109961635+Nagender-j@users.noreply.github.com>
2026-09-07 22:54:30 +0530

    Time: 4 ms (23.13%) | Memory: 9.5 MB (48.79%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int distinctSubseqII(string s) {
+        int n = s.size();
+        vector<int>last(26, -1);
+        vector<int>dp(n+1, 0);
+        dp[0] = 1;
+        
+        for(int i=1; i <=n; i++) {
+            // dp[i-1] %= mod;
+            dp[i] = (dp[i-1]%mod)*2;
+            dp[i] %= mod;
+            int charPos = s[i-1] - 'a';
+            if(last[charPos] != -1) {
+                dp[i] -= (dp[last[charPos]] % mod);
+            }
+            
+            if(dp[i] <= 0) dp[i] += mod;
+
+            last[charPos] = i-1;
+        }
+
+        return dp[n] -1 ;
+    }
+};
