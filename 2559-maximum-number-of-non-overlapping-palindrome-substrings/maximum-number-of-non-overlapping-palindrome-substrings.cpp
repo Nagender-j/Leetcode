5828fe4357d9546465376142baf1ddb6f9c221cf
Nagender-j <109961635+Nagender-j@users.noreply.github.com>
2026-09-15 23:08:00 +0530

    Time: 210 ms (35.34%) | Memory: 26.4 MB (27.49%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,51 @@
+struct interval {
+    int start, end;
+};
+
+class Solution {
+public:
+    int dp[2001][2001];
+    int isPalindrome(int l, int r, string &s) {
+        if(l >= r) return 1;
+        
+        if(dp[l][r] != -1) return dp[l][r];
+
+        if(s[l] != s[r]) return dp[l][r] = 0;
+        return dp[l][r] = isPalindrome(l+1, r-1, s);
+
+    } 
+    int maxPalindromes(string s, int k) {
+        memset(dp, -1, sizeof(dp));
+        int n = s.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n ; j++) {
+                dp[i][j] = isPalindrome(i, j, s);
+            }
+        }
+
+        return getAllPalindromes(n, k, s);
+    
+    }
+
+
+
+    int getAllPalindromes(int n, int k,string s) {
+        int cnt = 0;
+        int prev = -1;
+        for(int j = k-1; j < n ; j++) {
+            for(int i = j; i > prev ; i--) {
+                if(j-i+1 < k) continue;
+                if(dp[i][j] == 1) {
+                    cnt++;
+                    prev = j;
+                    
+                    break;
+                
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
