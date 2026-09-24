15c3b0656840ad9d59514c709c810b961c8a8b2d
Nagender-j <109961635+Nagender-j@users.noreply.github.com>
2026-09-24 23:54:40 +0530

    Time: 1 ms (99.69%) | Memory: 45.8 MB (24.38%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0 ; i < n; i++) {
+            int num = nums[i];
+            int sum = 0;
+            while(num > 0) {
+                int r = num%10;
+                num/=10;
+                sum += r;
+            }
+
+            if(i == sum) return i;
+        }
+
+        return -1;
+        
+    }
+}
